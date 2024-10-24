752de488b7f1dc13a8096ecf2e24c10552ad3e91
Konstantin <shirshikk@gmail.com>
2024-10-24 22:47:55 +0500

    widget function

---
+++ src/widget.py
@@ -0,0 +1,22 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card_info: str) -> str:
+    """Функция, которая обрабатывает информачию о картах и счетах"""
+    original_number = card_info.split()[-1]
+    if len(original_number) == 16:
+        card_number = get_mask_card_number(original_number)
+        result = f"{card_info[:-16]}{card_number}"
+    elif len(original_number) == 20:
+        account_number = get_mask_account(original_number)
+        result = f"{card_info[:-20]}{account_number}"
+    return result
+
+
+def get_date(date_info: str) -> str:
+    """Функция, которая принимает дату в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
+    year, month, day = date_info.split("T")[0].split("-")
+    return f"{day}.{month}.{year}"
+
+print(get_date("2024-03-11T02:26:18.671407"))
