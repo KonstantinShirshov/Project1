73855287886b4df98bcd3b1483abbceef938bd15
Konstantin <shirshikk@gmail.com>
2024-10-24 22:54:28 +0500

    homework after check errors

--- src/widget.py
+++ src/widget.py
@@ -19,4 +19,5 @@ def get_date(date_info: str) -> str:
     year, month, day = date_info.split("T")[0].split("-")
     return f"{day}.{month}.{year}"
 
+
 print(get_date("2024-03-11T02:26:18.671407"))
