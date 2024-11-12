817a3eb81fc4da458ed13b3bce5a525d66706203
Konstantin <shirshikk@gmail.com>
2024-11-12 23:12:35 +0500

    commit after linters

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,19 +1,20 @@
 import pytest
-from src.masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
+
 
 @pytest.fixture
-def numbers_of_card():
+def numbers_of_card() -> str:
     return "7000792289606361"
 
 
-def test_get_mask_card_number(numbers_of_card):
+def test_get_mask_card_number(numbers_of_card: str) -> None:
     assert get_mask_card_number(numbers_of_card) == "7000 79** **** 6361"
 
 
-@pytest.mark.parametrize("account_numbers, expected", [("98543355687898542153", "**2153"),
-                                                       ("73654108430135874305", "**4305"),
-                                                       ("43434434343454548779", "**8779")])
-
-
-def test_get_mask_account(account_numbers, expected):
+@pytest.mark.parametrize(
+    "account_numbers, expected",
+    [("98543355687898542153", "**2153"), ("73654108430135874305", "**4305"), ("43434434343454548779", "**8779")],
+)
+def test_get_mask_account(account_numbers: str, expected: str) -> None:
     assert get_mask_account(account_numbers) == expected
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,8 +1,10 @@
 import pytest
 
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
-@pytest.mark.parametrize("card_info, expected",
+
+@pytest.mark.parametrize(
+    "card_info, expected",
     [
         ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
         ("Счет 64686473678894779589", "Счет **9589"),
@@ -10,11 +12,10 @@ from src.widget import mask_account_card, get_date
         ("Счет 35383033474447895560", "Счет **5560"),
         ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
         ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
-        ("Счет 73654108430135874305", "Счет **4305")
+        ("Счет 73654108430135874305", "Счет **4305"),
     ],
 )
-
-def test_mask_account_card(card_info, expected):
+def test_mask_account_card(card_info: str, expected: str) -> None:
     assert mask_account_card(card_info) == expected
 
 
